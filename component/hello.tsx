c95af9a9b6efe2cf46387b91b2ba8bc59e09194b
Juls <juliograterolb@gmail.com>
2026-03-19 11:42:10 -0400

    changed the greeting for accuracy of hours

--- component/hello.tsx
+++ component/hello.tsx
@@ -3,7 +3,6 @@
 import { useEffect, useState } from "react";
 
 export default function Hello({ name = "User" }: { name?: string }) {
-  const greeting: string[] = ["Good morning", "Good afternoon"];
   const message: string[] = [
     "How can I help you?",
     "What can I do for you today?",
@@ -12,19 +11,32 @@ export default function Hello({ name = "User" }: { name?: string }) {
   ];
 
   const [randomMessage, setRandomMessage] = useState("");
-  const [randomGreeting, setRandomGreeting] = useState("");
+  const [greeting, setGreeting] = useState("Hello");
 
   useEffect(() => {
+    // Get user's local hour
+    const hour = new Date().getHours();
+
+    let greetingText = "Hello";
+
+    if (hour < 12) {
+      greetingText = "Good morning";
+    } else if (hour < 18) {
+      greetingText = "Good afternoon";
+    } else {
+      greetingText = "Good evening";
+    }
+
+    setGreeting(greetingText);
+
     setRandomMessage(message[Math.floor(Math.random() * message.length)]);
-    setRandomGreeting(greeting[Math.floor(Math.random() * greeting.length)]);
   }, []);
 
   return (
     <div className="flex flex-col items-center gap-2">
       <h1 className="md:text-5xl text-4xl text-white text-center font-medium tracking-[-7.5%]">
-        {randomGreeting}, {name}
+        {greeting}, {name}
       </h1>
-
       <p className="text-lg text-zinc-400 text-center">{randomMessage}</p>
     </div>
   );
